5a1192c7ceb79185a61aec99c66a85f0310b5663
Randy Reed <randywreed@gmail.com>
2020-06-08 23:10:50 -0400

    more changes to csv

--- wikitext-103.py
+++ wikitext-103.py
@@ -1,7 +1,27 @@
 #wikitext-103
 import csv
-with open("/home/randy/Downloads/wikitext-103/wiki.train.tokens") as csv_file:
+import re
+pattern=re.compile("^\s=\s[a-zA-Z\s]+=")
+outputfile='/home/randy/Downloads/wikitext-103/all_articles.csv'
+f_out=open (outputfile, "w")
+article_flag=False
+txt=""
+with open("/home/randy/Downloads/wikitext-103/wiki.train.tokens.doc") as csv_file, f_out:
     csv_reader =csv.reader(csv_file)
+    csv_writer=csv.writer(f_out)
     for row in csv_reader:
-        print(row)
-        input("wait")
+        for i in row:
+            if pattern.match(i):
+                if article_flag==True:
+                #write out
+                    csv_writer.writerow([j,txt])
+                    txt=""
+                j=i.replace('=','').strip(' ')
+                article_flag=True
+                
+            else:
+                txt=txt+i
+
+        
+print('alldone')
+                
